58afb0474f4feba0bec56ec184e32a760a7941f6
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2024-02-11 19:21:47 +0600

    data shown dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [user, setUser] = useState([]);
-  console.log(user);
+  const [user, setUser] = useState({});
+  
   useEffect(() => {
     fetch('https://randomuser.me/api/?page=1&results=1&seed=abc')
       .then((res) => res.json())
-      .then((data) => setUser(data));
+      .then((data) => setUser(data.results[0]));
   }, []);
+  
   return (
     <div className="flex justify-center items-center mt-60">
       <div className="card w-[580px] card-side bg-gray-200 text-black shadow-xl">
         <figure>
           <img
             className="p-5"
-            src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp"
-            alt="ID"
+            src={user.picture ? user.picture.large : ''}
+            alt="User"
           />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">John Doe</h2>
-          <p>Male</p>
-          <p>+880 1912 484 5590</p>
+          <h2 className="card-title">{`${user.name?.title} ${user.name?.first} ${user.name?.last}`}</h2>
+          <p>{user.gender}</p>
+          <p>{user.phone}</p>
         </div>
       </div>
     </div>
